e85e7b70560f5a88a3c93975a6a99c6bf992643b
kami.wm <kami.wm@alibaba-inc.com>
2017-10-17 21:08:28 +0800

    m: 修改reader的迭代器

--- src/pipeline/reader.py
+++ src/pipeline/reader.py
@@ -19,48 +19,80 @@ def reader_csv(path: str) -> (np.array, np.array):
     return x.values, y.values
 
 
-def generate(x: np.array, y: np.array, batch_size: int, num_steps, name: str = None):
+def generate(sample: np.array, sample_name: str, batch_size: int, name_scope: str = None):
     """ 返回数据的迭代器
   Args:
-    raw_data: one of the raw data outputs from ptb_raw_data.
-    batch_size: int, the batch size.
-    num_steps: int, the number of unrolls.
-    name: the name of this operation (optional).
+    :param sample_name:
+    :param sample:
+    :param batch_num: batch的数目
+    :param name_scope:
   Returns:
       两个迭代器
   Raises:
     tf.errors.InvalidArgumentError: if batch_size or num_steps are too high.
   """
-    with tf.name_scope(name, "Producer", [x, y, batch_size, num_steps]):
-        x = tf.convert_to_tensor(x, name="x", dtype=tf.int32)
-        y = tf.convert_to_tensor(y, name="y", dtype=tf.int32)
+    with tf.name_scope(name_scope, "Producer", [sample, batch_size]):
+        sample = tf.convert_to_tensor(sample, name=sample_name, dtype=tf.int32)
 
-        data_len = tf.size(y)
-        batch_len = data_len // batch_size
-
-        x = tf.reshape(x[0: batch_size * batch_len, :], [batch_size, batch_len])
-        y = tf.reshape(y[0: batch_size * batch_len], [batch_size, batch_len])
-
-        epoch_size = (batch_len - 1) // num_steps
+        # 获取样本的个数
+        sample_num = tf.size(y)
+        batch_num = sample_num // batch_size
         assertion = tf.assert_positive(
-            epoch_size,
-            message="epoch_size == 0, decrease batch_size or num_steps")
-
+            batch_num,
+            message="batch_num == 0, decrease batch_size"
+        )
         with tf.control_dependencies([assertion]):
-            epoch_size = tf.identity(epoch_size, name="epoch_size")
+            batch_size = tf.identity(batch_size, name="batch_size")
 
-        i = tf.train.range_input_producer(epoch_size, shuffle=False).dequeue()
-        x = tf.strided_slice(data, [0, i * num_steps],
-                             [batch_size, (i + 1) * num_steps])
-        x.set_shape([batch_size, num_steps])
-        y = tf.strided_slice(data, [0, i * num_steps + 1],
-                             [batch_size, (i + 1) * num_steps + 1])
-        y.set_shape([batch_size, num_steps])
-        return x, y
+        sample_dim = [d.value for d in sample.shape[1:]]
+
+        # 原数据数目: batch_nums * batch_len,
+        if len(sample_dim) == 0:
+            sample = tf.reshape(sample[0: batch_num * batch_size], [batch_num, batch_size])
+        else:
+            sample = tf.reshape(sample[0: batch_num * batch_size], [batch_num, batch_size, *sample_dim])
+
+        print(sample.shape)
+
+        i = tf.train.range_input_producer(batch_num, shuffle=False).dequeue()
+
+        if len(sample_dim) == 0:
+            sample_bacth = tf.strided_slice(sample, [i * batch_size], [(i+1) * batch_size + 1])
+        else:
+            sample_bacth = tf.strided_slice(sample, [i * batch_size], [(i+1) * batch_size + 1])
+
+        # if len(sample_dim) == 0:
+        #     sample_bacth.set_shape([batch_num, batch_len])
+        # else:
+        #     sample_bacth.set_shape([batch_num, batch_len, *sample_dim])
+
+        return sample_bacth
 
 
 if __name__ == '__main__':
-    data_path = '/Users/kami.wm/DataScience/data/kaggle-porto-seguros-safe-driver-prediction/train_ps_ind.csv'
+    from setup import config
+    data_path = config.data.path('train_ps_ind.csv')
     x, y = reader_csv(data_path)
-    print(x, y)
 
+    # x = np.asarray(list(range(0, 1024*8)), dtype=np.int32).reshape([1024,8])
+    # y = np.asarray(list(range(0, 1024)), dtype=np.int32)
+
+    x_batch = generate(sample=x, sample_name="x", batch_size=1, name_scope="123")
+    y_batch = generate(sample=y, sample_name="y", batch_size=1, name_scope="123")
+
+    with tf.Session() as session:
+        coord = tf.train.Coordinator()
+        tf.train.start_queue_runners(session, coord=coord)
+        try:
+            xval, yval = session.run([x_batch, y_batch])
+            print(xval)
+            print(yval)
+            xval, yval = session.run([x_batch, y_batch])
+            print(xval)
+            print(yval)
+            xval, yval = session.run([x_batch, y_batch])
+            print(xval)
+            print(yval)
+        finally:
+            coord.request_stop()
+            coord.join()
