dbfa23c7fb05c5b5112692207b1cffd8f773e6ed
kami.wm <kami.wm@alibaba-inc.com>
2017-10-26 21:18:09 +0800

    全量处理

--- preprocesss/utils.py
+++ preprocesss/utils.py
@@ -1,3 +1,62 @@
 """
 预处理常用工具类
 """
+
+import pandas as pd
+
+
+# 获取某一类的特征列名
+def get_column_name_list_by_prefix(df: pd.DataFrame, prefix: str) -> list:
+    columns = list(df.columns)
+    return [col for col in columns if col.startswith(prefix)]
+
+
+# 检查特征列里有没有缺失值
+def get_column_name_list_with_missing_value(df: pd.DataFrame, feature: list, placeholder) -> list:
+    col_has_missing_value = []
+    for col in feature:
+        value_counts = df[col].value_counts()
+        value_counts_index = list(value_counts.index)
+        if placeholder in value_counts_index:
+            col_has_missing_value.append(col)
+    return col_has_missing_value
+
+
+def fix_feature_ps_ind(df: pd.DataFrame) -> pd.DataFrame:
+    # 存在缺失值的都是 category, 直接 one-hot编码
+    df_columns = list(df.columns)
+    _df = pd.get_dummies(
+        data=df,
+        columns=[col for col in df_columns if '_cat' in col]
+    )
+
+    return _df
+
+
+# 给每个列加上一个基数
+def indexing(df: pd.DataFrame, columns: list, offset: int = 0, neat: bool = True) -> (pd.DataFrame, int):
+    df = df.copy()
+
+    for col in columns:
+        # 如果要精简, 每列先减去其最小值
+
+        col_min = df[col].min()
+        col_max = df[col].max()
+
+        col_value_range_length = col_max - col_min + 1
+
+        # 每列减去最小值
+        if col_min != 0 and neat:
+            df[col] = df[col] - col_min
+
+        col_unique_value = sorted(pd.unique(df[col]))
+        col_unique_value_number = \
+            len(col_unique_value) if col_value_range_length <= len(col_unique_value) else col_value_range_length
+
+        print(col, col_max, col_min, col_unique_value_number, col_unique_value)
+
+        df[col] = df[col] + offset
+        offset = offset + col_unique_value_number
+
+    return df, offset
+
--- src/ind-lstm-ensemble-avg.py
+++ src/ind-lstm-ensemble-avg.py
@@ -7,12 +7,12 @@ from src.pipeline.reader import reader_csv
 data_lstm_resullt_path = config.data.path('predict-ind-lstm-with-embedding')
 data_lstm_ensemble_avg_path = config.data.path('predict-ind-lstm-with-embedding.ensemble.avg.result.csv')
 
-__, x, _ = reader_csv(data_lstm_resullt_path)
+ID, x, _ = reader_csv(data_lstm_resullt_path)
 
 _y = np.mean(x, axis=1)
 
-test_data_path = config.data.path('test_ind_uniquified.csv')
-ID, _, _ = reader_csv(test_data_path)
+# test_data_path = config.data.path('test_ind_uniquified.csv')
+# ID, _, _ = reader_csv(test_data_path)
 
 data_lstm_ensemble_pd = pd.DataFrame({'id': ID, 'target': _y})
 
