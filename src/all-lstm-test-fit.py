547faacd87be6e7c5bda69f7da1e2699ba07f2dd
ekansrm <ekansrm@gmail.com>
2017-11-07 08:58:26 +0800

    +: 一个早merge的lstm模型

--- src/all-lstm-test-fit.py
+++ src/all-lstm-test-fit.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     ####################################################################################################################
     # 数据全局变量
     data_clip_path = config.data.path('data_indexed_train_clip.csv')
-    data_path = config.data.path('data_indexed_train_clip.csv')
+    data_path = config.data.path('data_indexed_train.csv')
     embedding_index_offset = config.parameter.get('embedding.index.offset')
     embedding_index_length = config.parameter.get('embedding.index.length')
     embedding_word_number = embedding_index_length - embedding_index_offset
@@ -35,13 +35,18 @@ if __name__ == '__main__':
     model_path = config.runtime.path(model_name)
     model_path_tag = model_name + '.%d.save'
     model_checkpoint_path = config.runtime.path(
-        model_name + '.%d.checkpoint'
+        model_name + '.checkpoint'
                      '.epoch-{epoch:02d}'
                      '.val_loss-{val_loss:.6f}'
                      '.val_acc-{val_acc:.6f}'
     )
 
-    model_checkpoint_best_path = config.runtime.path(model_name + '.checkpoint.best')
+    model_checkpoint_best_path = config.runtime.path(
+        model_name + '.checkpoint.best'
+                     '.epoch-{epoch:02d}'
+                     '.val_loss-{val_loss:.6f}'
+                     '.val_acc-{val_acc:.6f}'
+    )
     model_checkpoint_best_path_tag = model_name + '.checkpoint.best.save'
 
     model_runtime_data_path = config.runtime.path(model_name + '.data.%d')
@@ -64,117 +69,117 @@ if __name__ == '__main__':
 
     ####################################################################################################################
 
-    if not flag_running:
-        data_clip = pd.read_csv(data_clip_path, low_memory=True)
+    data_clip = pd.read_csv(data_clip_path, low_memory=True)
 
-        feature_column_type = preprocess_utils.get_column_type_pair_list(df=data_clip, prefix='ps_')
+    feature_column_type = preprocess_utils.get_column_type_pair_list(df=data_clip, prefix='ps_')
 
-        column_name_list_feature_type_int = [
-            x[0]
-            for x in filter(
-                lambda x: 'float' not in x[1],
-                feature_column_type
-            )
-        ]
-        feature_ps_ind = sorted([x for x in column_name_list_feature_type_int if 'ps_ind' in x])
-        feature_ps_calc = sorted([x for x in column_name_list_feature_type_int if 'ps_calc' in x])
-        feature_ps_reg = sorted([x for x in column_name_list_feature_type_int if 'ps_reg' in x])
-        feature_ps_car = sorted([x for x in column_name_list_feature_type_int if 'ps_car' in x])
-        assert len(column_name_list_feature_type_int) == \
-            len(feature_ps_ind + feature_ps_calc + feature_ps_reg + feature_ps_car)
-        column_name_list_feature_type_int = feature_ps_ind + feature_ps_calc + feature_ps_reg + feature_ps_car
+    column_name_list_feature_type_int = [
+        x[0]
+        for x in filter(
+            lambda x: 'float' not in x[1],
+            feature_column_type
+        )
+    ]
+    feature_ps_ind = sorted([x for x in column_name_list_feature_type_int if 'ps_ind' in x])
+    feature_ps_calc = sorted([x for x in column_name_list_feature_type_int if 'ps_calc' in x])
+    feature_ps_reg = sorted([x for x in column_name_list_feature_type_int if 'ps_reg' in x])
+    feature_ps_car = sorted([x for x in column_name_list_feature_type_int if 'ps_car' in x])
+    assert len(column_name_list_feature_type_int) == \
+        len(feature_ps_ind + feature_ps_calc + feature_ps_reg + feature_ps_car)
+    column_name_list_feature_type_int = feature_ps_ind + feature_ps_calc + feature_ps_reg + feature_ps_car
 
-        column_name_list_feature_type_float = [
-            x[0]
-            for x in filter(
-                lambda x: 'float' in x[1],
-                feature_column_type
-            )
-        ]
-        column_name_list_feature_type_float = sorted(column_name_list_feature_type_float, reverse=True)
+    column_name_list_feature_type_float = [
+        x[0]
+        for x in filter(
+            lambda x: 'float' in x[1],
+            feature_column_type
+        )
+    ]
+    column_name_list_feature_type_float = sorted(column_name_list_feature_type_float, reverse=True)
 
-        dtype_dict = {
-            'id': 'uint32',
-            'target': 'float32',
-            'set': 'str'
+    dtype_dict = {
+        'id': 'uint32',
+        'target': 'float32',
+        'set': 'str'
+    }
+    dtype_dict.update(
+        {
+            k: 'float32' for k in column_name_list_feature_type_float
         }
-        dtype_dict.update(
-            {
-                k: 'float32' for k in column_name_list_feature_type_float
-            }
-        )
-        dtype_dict.update(
-            {
-                k: 'uint8' for k in column_name_list_feature_type_int
-            }
-        )
+    )
+    dtype_dict.update(
+        {
+            k: 'uint8' for k in column_name_list_feature_type_int
+        }
+    )
 
-        data = pd.read_csv(data_path, dtype=dtype_dict, low_memory=True)
-        data = data.sample(n=3000)
-        data_train = data[data['set'] == 'train']
+    data = pd.read_csv(data_path, dtype=dtype_dict, low_memory=True)
+    data_train = data[data['set'] == 'train']
 
-        config.parameter.put(model_name + ".columns.int", column_name_list_feature_type_int)
-        config.parameter.put(model_name + ".columns.float", column_name_list_feature_type_float)
+    config.parameter.put(model_name + ".columns.int", column_name_list_feature_type_int)
+    config.parameter.put(model_name + ".columns.float", column_name_list_feature_type_float)
 
-        x_int = data_train[column_name_list_feature_type_int].values
-        x_float = data_train[column_name_list_feature_type_float].values
-        y = data_train['target'].values
-        ID = data_train['id'].values
+    x_int = data_train[column_name_list_feature_type_int].values
+    x_float = data_train[column_name_list_feature_type_float].values
+    y = data_train['target'].values
+    ID = data_train['id'].values
 
-        ## 构造训练数据
+    ## 构造训练数据
 
-        idx_pos = y == 1
-        x_int_pos = x_int[idx_pos]
-        x_float_pos = x_float[idx_pos]
-        y_pos = y[idx_pos]
-        ID_pos = ID[idx_pos]
-        size_pos = len(y_pos)
-        print("正例数目: ", size_pos)
+    idx_pos = y == 1
+    x_int_pos = x_int[idx_pos]
+    x_float_pos = x_float[idx_pos]
+    y_pos = y[idx_pos]
+    ID_pos = ID[idx_pos]
+    size_pos = len(y_pos)
+    print("正例数目: ", size_pos)
 
-        idx_neg = y == 0
-        x_int_neg = x_int[idx_neg]
-        x_float_neg = x_float[idx_neg]
-        y_neg = y[idx_neg]
-        ID_neg = ID[idx_neg]
-        size_neg = len(y_neg)
-        print("反例数目: ", size_neg)
+    idx_neg = y == 0
+    x_int_neg = x_int[idx_neg]
+    x_float_neg = x_float[idx_neg]
+    y_neg = y[idx_neg]
+    ID_neg = ID[idx_neg]
+    size_neg = len(y_neg)
+    print("反例数目: ", size_neg)
 
-        # 计算需要的估计器
-        n_estimator = size_neg // size_pos
-        print("估计器数目: ", n_estimator)
-        config.parameter.put(model_name + '.estimator.numbers', n_estimator)
+    # 计算需要的估计器
+    n_estimator = size_neg // size_pos
+    config.parameter.put(model_name + '.estimator.numbers', n_estimator)
 
-        # 构造模型, 逐个训练
+    seg_neg_begin = list(range(0, size_neg, size_pos))
+    seg_neg_end = list(seg_neg_begin)
+    if len(seg_neg_begin) != 0:
+        seg_neg_end.pop(0)
+        seg_neg_end.append(size_neg)
+    seg_neg = list(zip(seg_neg_begin, seg_neg_end))
+    seg_neg = seg_neg[0: n_estimator]
 
-        seg_neg_begin = list(range(0, size_neg, size_pos))
-        seg_neg_end = list(seg_neg_begin)
-        if len(seg_neg_begin) != 0:
-            seg_neg_end.pop(0)
-            seg_neg_end.append(size_neg)
-        seg_neg = list(zip(seg_neg_begin, seg_neg_end))
-        seg_neg = seg_neg[0: n_estimator]
+    x_int = None
+    x_float = None
+    y = None
+    Id = None
+    for i, (b, e) in enumerate(seg_neg):
+        _x_int = np.concatenate((x_int_pos, x_int_neg[b:e]), axis=0)
+        _x_float = np.concatenate((x_float_pos, x_float_neg[b:e]), axis=0)
+        _y = np.concatenate((y_pos, y_neg[b:e]), axis=0)
+        _id = np.concatenate((ID_pos, ID_neg[b:e]), axis=0)
 
-        for i, (b, e) in enumerate(seg_neg):
-            _x_int = np.concatenate((x_int_pos, x_int_neg[b:e]), axis=0)
-            _x_float = np.concatenate((x_float_pos, x_float_neg[b:e]), axis=0)
-            _y = np.concatenate((y_pos, y_neg[b:e]), axis=0)
-            _id = np.concatenate((ID_pos, ID_neg[b:e]), axis=0)
-            permutation = np.random.permutation(_y.shape[0])
-            _x_int = _x_int[permutation]
-            _x_float = _x_float[permutation]
-            _y = _y[permutation]
-            _id = _id[permutation]
-            with open((model_runtime_data_path+'.neg') % i, 'wb') as fp:
-                pickle.dump((_x_int, _x_float, _y), fp, 1)
+        if i == 0:
+            x_int = _x_int
+            x_float = _x_float
+            y = _y
+            Id = _id
+        else:
+            x_int = np.concatenate((x_int, _x_int), axis=0)
+            x_float = np.concatenate((x_float, _x_float), axis=0)
+            y = np.concatenate((y, _y), axis=0)
+            Id = np.concatenate((Id, _id), axis=0)
 
-
-    else:
-        # 从文件恢复数据
-        n_estimator = config.parameter.get(model_name + '.estimator.numbers')
-        column_name_list_feature_type_int = config.parameter.get(model_name + ".columns.int")
-        column_name_list_feature_type_float = config.parameter.get(model_name + ".columns.float")
-
-    # 如果是训练, 恢复配置好的数据, 否则, 重新加载数据, 并保存为文件
+        permutation = np.random.permutation(y.shape[0])
+        x_int = x_int[permutation]
+        x_float = x_float[permutation]
+        y = y[permutation]
+        Id = Id[permutation]
 
     ####################################################################################################################
     # 模型构造
@@ -198,14 +203,11 @@ if __name__ == '__main__':
         return lrate
     lrate = LearningRateScheduler(step_decay)
 
-    early_stopping = EarlyStopping(monitor='val_acc', min_delta=0.0001, mode='max', patience=2, verbose=1)
+    early_stopping = EarlyStopping(monitor='val_acc', min_delta=0.0001, mode='max', patience=3, verbose=1)
 
     ####################################################################################################################
     # Run !
-    if flag_running:
-        print('断点继续, step={0}'.format(breakpoint))
 
-    breakpoint = 0
     config.parameter.put(model_runtime_flag_running_tag, True)
     lstm = EmbeddedLSTM()
     lstm.config = embedded_lstm_config
@@ -219,37 +221,22 @@ if __name__ == '__main__':
         loss_weights={'y': 1., },
         metrics=['accuracy']
     )
-    for i in range(0, n_estimator):
 
-        if i < breakpoint:
-            continue
+    config.parameter.put(model_runtime_breakpoint_tag, i)
 
-        if i > 2:
-            break
+    checkpoint = ModelCheckpoint(
+        model_checkpoint_path, save_best_only=False, verbose=1)
 
-        print('='*120)
-        print('训练估计器... {0}/{1}'.format(i, n_estimator))
-        print('加载训练数据...')
-        with open(model_runtime_data_path % i, 'rb') as fp:
-            (_x, _x_float, _y) = pickle.load(fp)
+    checkpoint_best = ModelCheckpoint(
+        model_checkpoint_best_path, save_best_only=True, monitor='val_acc',  mode='max', verbose=1)
 
-        config.parameter.put(model_runtime_breakpoint_tag, i)
+    lstm.model.fit(
+        x={'x_int': x_int, 'x_float': x_float},
+        y={'y': y},
+        epochs=200, batch_size=64, shuffle=True, validation_split=0.2, verbose=1,
+        callbacks=[checkpoint_best, early_stopping]
+    )
 
-        checkpoint = ModelCheckpoint(
-            model_checkpoint_path, save_best_only=False, verbose=1)
-
-        checkpoint_best = ModelCheckpoint(
-            model_checkpoint_best_path, save_best_only=True, monitor='val_acc',  mode='max', verbose=1)
-
-        lstm.model.fit(
-            x={'x_int': _x, 'x_float': _x_float},
-            y={'y': _y},
-            epochs=200, batch_size=64, shuffle=True, validation_split=0.2, verbose=1,
-            callbacks=[checkpoint_best]
-        )
-
-        lstm.model.save(model_path % i)
-
-        print('='*120)
+    lstm.model.save(model_path % i)
 
     config.parameter.put(model_runtime_flag_running_tag, False)
